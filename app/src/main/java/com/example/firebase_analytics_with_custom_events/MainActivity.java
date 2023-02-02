ab4cfb62ee31268f0012ade8104b480d08b74989
jaypithavaelsner <jaypithavaelsner@gmail.com>
2023-02-02 22:49:12 +0530

    Analytics dependency added.

--- app/src/main/java/com/example/firebase_analytics_with_custom_events/MainActivity.java
+++ app/src/main/java/com/example/firebase_analytics_with_custom_events/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAnalytics analytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        analytics= FirebaseAnalytics.getInstance(this);
+
+
     }
 }
