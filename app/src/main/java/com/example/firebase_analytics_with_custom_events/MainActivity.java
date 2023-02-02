ff17f6be1341be0df90375386fa1bd546858fb68
jaypithavaelsner <jaypithavaelsner@gmail.com>
2023-02-02 22:40:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebase_analytics_with_custom_events/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebase_analytics_with_custom_events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
