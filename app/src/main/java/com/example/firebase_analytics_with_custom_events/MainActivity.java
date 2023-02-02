b8167593d664fca92c9ade15618d9f6d81030336
jaypithavaelsner <jaypithavaelsner@gmail.com>
2023-02-02 23:23:34 +0530

    Log Event Added

--- app/src/main/java/com/example/firebase_analytics_with_custom_events/MainActivity.java
+++ app/src/main/java/com/example/firebase_analytics_with_custom_events/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.firebase_analytics_with_custom_events;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAnalytics analytics;
+    Button addToCart_btn,cod_btn,directTransfer_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,36 @@ public class MainActivity extends AppCompatActivity {
 
         analytics= FirebaseAnalytics.getInstance(this);
 
+        addToCart_btn=findViewById(R.id.addToCart_btn);
+        cod_btn=findViewById(R.id.cod_btn);
+        directTransfer_btn=findViewById(R.id.directTransfer_btn);
+
+        addToCart_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle=new Bundle();
+                bundle.putString("Cart","Cart Button");
+                analytics.logEvent("Cart_Button",bundle);
+            }
+        });
+
+        cod_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle=new Bundle();
+                bundle.putString("Cod","Cod Button");
+                analytics.logEvent("Cod_Button",bundle);
+            }
+        });
+
+        directTransfer_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle=new Bundle();
+                bundle.putString("directTransfer","directTransfer Button");
+                analytics.logEvent("directTransfer_Button",bundle);
+            }
+        });
 
     }
 }
